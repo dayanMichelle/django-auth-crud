5a7fd21844faac6a8d22de9c7706c3dc9de2d193
Dayan Arango <dayaara6@gmail.com>
2022-12-07 10:14:08 -0300

    r

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -83,12 +83,11 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        dj_database_url.config(
+    'default': dj_database_url.config(
             default='postgresql://postgres:postgres@localhost/postgres',
             conn_max_age=600
         )
-    }
+
 }
 
 
